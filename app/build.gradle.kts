a9b15711f79eb2027c3b99b560005740580a6431
NedenSinir <123alperozyurt@gmail.com>
2025-10-04 13:45:56 +0200

    fix: update SonarCloud project configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,12 +93,12 @@ android {
 
 sonar {
     properties {
-        property("sonar.projectKey", "gf_android-sample")
-        property("sonar.projectName", "Android-Sample")
-        property("sonar.organization", "gabrielfleischer")
+        property("sonar.projectKey", "SkillBridgeee_SkillBridgeee")
+        property("sonar.projectName", "SkillBridgeee")
+        property("sonar.organization", "SkillBridgeee")
         property("sonar.host.url", "https://sonarcloud.io")
         // Comma-separated paths to the various directories containing the *.xml JUnit report files. Each path may be absolute or relative to the project base directory.
-        property("sonar.junit.reportPaths", "${project.layout.buildDirectory.get()}/test-results/testDebugunitTest/")
+        property("sonar.junit.reportPaths", "${project.layout.buildDirectory.get()}/test-results/testDebugUnitTest/")
         // Paths to xml files with Android Lint issues. If the main flavor is changed, this file will have to be changed too.
         property("sonar.androidLint.reportPaths", "${project.layout.buildDirectory.get()}/reports/lint-results-debug.xml")
         // Paths to JaCoCo XML coverage report files.
