35b96c31025e1271d2beb8a286c6024369561517
nahuelArthur <160842802+nahuelArthur@users.noreply.github.com>
2025-10-09 11:13:36 +0200

    feat: add newSkill overviewModel
    Implementation of the basic overviewModel functionality.
    Category and kind are not yet supported

--- app/src/main/java/com/android/sample/ui/newSkill/NewSkillOverviewModel.kt
+++ app/src/main/java/com/android/sample/ui/newSkill/NewSkillOverviewModel.kt
@@ -1,2 +1,100 @@
-package com.android.sample.ui.NewSkill
+package com.android.sample.ui.newSkill
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+
+
+
+/** UI state for the MyProfile screen. This state holds the data needed to edit a profile */
+data class SkillUIState(
+    val ownerId: String = "John Doe",
+    val title: String = "super skill",
+    val description: String = "jsp gros",
+    val price: String = "2000",
+
+    val errorMsg: String? = null,
+    val invalidTitleMsg: String? = null,
+    val invalidDescMsg: String? = null,
+    val invalidPriceMsg: String? = null,
+) {
+    val isValid: Boolean
+        get() =
+            invalidTitleMsg == null &&
+                    invalidDescMsg == null &&
+                    invalidPriceMsg == null &&
+                    title.isNotEmpty() &&
+                    description.isNotEmpty()
+}
+
+class NewSkillOverviewModel() : ViewModel() {
+    // Profile UI state
+    private val _uiState = MutableStateFlow(SkillUIState())
+    val uiState: StateFlow<SkillUIState> = _uiState.asStateFlow()
+
+    /** Clears the error message in the UI state. */
+    fun clearErrorMsg() {
+        _uiState.value = _uiState.value.copy(errorMsg = null)
+    }
+
+
+
+    fun loadSkill() {
+        viewModelScope.launch {
+            try {
+
+            } catch (_: Exception) {
+
+            }
+        }
+    }
+
+
+    // Functions to update the UI state.
+
+    fun setTitle(title: String) {
+        _uiState.value =
+            _uiState.value.copy(
+                title = title,
+                invalidTitleMsg = if (title.isBlank()) "Title cannot be empty" else null)
+    }
+
+    fun setDesc(description: String) {
+        _uiState.value =
+            _uiState.value.copy(
+                description = description,
+                invalidDescMsg =
+                    if (description.isBlank())
+                        "Description cannot be empty"
+                    else null
+            )
+    }
+
+    fun setPrice(price: String) {
+        _uiState.value =
+            _uiState.value.copy(
+                price = price,
+                invalidPriceMsg =
+                    if (price.isBlank()) "Price cannot be empty"
+                    else if (!isNumber(price)) "Price must be a number"
+                    else null
+            )
+    }
+
+
+
+
+    private fun isNumber(num: String): Boolean {
+        return try {
+            val res = num.toDouble()
+            !res.isNaN() && (res >= 0.0)
+        } catch (_: Exception) {
+            false
+        }
+    }
+
+}
