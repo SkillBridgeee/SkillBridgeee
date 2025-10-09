262cd248b8321652d91b3182319c4e469a98d9bd
EBali2003 <eren.bali@epfl.ch>
2025-10-09 11:43:33 +0200

    added the comment because apparently it didn't get committed

--- app/src/main/java/com/android/sample/model/user/Profile.kt
+++ app/src/main/java/com/android/sample/model/user/Profile.kt
@@ -4,7 +4,10 @@ import com.android.sample.model.map.Location
 
 /** Data class representing user profile information */
 data class Profile(
-    /** I didn't change the userId request yet because according to my searches it would be better if we implement it with authentication*/
+    /**
+     * I didn't change the userId request yet because according to my searches it would be better if
+     * we implement it with authentication
+     */
     val userId: String = "",
     val name: String = "",
     val email: String = "",
