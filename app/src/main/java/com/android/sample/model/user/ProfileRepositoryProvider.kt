e2b905cbc9916b883e2aad88b03c33d83bc6b654
Zay1939 <nikita.karpachev@gmail.com>
2025-10-14 18:47:10 +0200

    merge stashed changes into current branch

--- app/src/main/java/com/android/sample/model/user/ProfileRepositoryProvider.kt
+++ app/src/main/java/com/android/sample/model/user/ProfileRepositoryProvider.kt
@@ -1,5 +1,6 @@
 package com.android.sample.model.user
 
+import com.android.sample.model.tutor.ProfileRepositoryLocal
 
 /** Provides a single instance of the TutorRepository (swap for a remote impl in prod/tests). */
 object ProfileRepositoryProvider {
