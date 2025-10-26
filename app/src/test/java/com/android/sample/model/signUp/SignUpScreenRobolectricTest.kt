d73f58821127ecf5c9745d09688ddd6085811d08
EBali2003 <eren.bali@epfl.ch>
2025-10-26 18:05:50 +0100

    fix robolectric tests for the sign up screen to have firebase.

--- app/src/test/java/com/android/sample/model/signUp/SignUpScreenRobolectricTest.kt
+++ app/src/test/java/com/android/sample/model/signUp/SignUpScreenRobolectricTest.kt
@@ -1,24 +1,61 @@
-package com.android.sample.ui.signup
+package com.android.sample.model.signUp
 
+import android.content.Context
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performTextInput
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.android.sample.model.user.FakeProfileRepository
+import com.android.sample.model.user.ProfileRepositoryProvider
+import com.android.sample.ui.signup.SignUpScreen
+import com.android.sample.ui.signup.SignUpScreenTestTags
+import com.android.sample.ui.signup.SignUpViewModel
 import com.android.sample.ui.theme.SampleAppTheme
+import com.google.firebase.FirebaseApp
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
 
 @RunWith(AndroidJUnit4::class)
+@Config(sdk = [28]) // Use SDK 28 for better compatibility
 class SignUpScreenRobolectricTest {
 
   @get:Rule val rule = createComposeRule()
 
+  @Before
+  fun setUp() {
+    // Initialize Firebase for Robolectric tests
+    val context = ApplicationProvider.getApplicationContext<Context>()
+
+    // Ensure any existing Firebase instance is cleared
+    try {
+      FirebaseApp.clearInstancesForTest()
+    } catch (_: Exception) {
+      // Ignore if clearInstancesForTest is not available
+    }
+
+    try {
+      FirebaseApp.initializeApp(context)
+    } catch (_: IllegalStateException) {
+      // Firebase already initialized
+    }
+
+    // Set up fake repository to avoid Firestore dependency
+    ProfileRepositoryProvider.setForTests(FakeProfileRepository())
+  }
+
   @Test
   fun renders_core_fields() {
-    val vm = SignUpViewModel()
-    rule.setContent { SampleAppTheme { SignUpScreen(vm = vm) } }
+    rule.setContent {
+      SampleAppTheme {
+        val vm = SignUpViewModel()
+        SignUpScreen(vm = vm)
+      }
+    }
 
     rule.onNodeWithTag(SignUpScreenTestTags.TITLE, useUnmergedTree = false).assertExists()
     rule.onNodeWithTag(SignUpScreenTestTags.NAME, useUnmergedTree = false).assertExists()
@@ -29,8 +66,12 @@ class SignUpScreenRobolectricTest {
 
   @Test
   fun entering_valid_form_enables_sign_up_button() {
-    val vm = SignUpViewModel()
-    rule.setContent { SampleAppTheme { SignUpScreen(vm = vm) } }
+    rule.setContent {
+      SampleAppTheme {
+        val vm = SignUpViewModel()
+        SignUpScreen(vm = vm)
+      }
+    }
 
     rule.onNodeWithTag(SignUpScreenTestTags.NAME, useUnmergedTree = false).performTextInput("Élise")
     rule
