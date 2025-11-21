a9f9cf8fba58da9090e061c55494036bf8fd4946
nahuelArthur <160842802+nahuelArthur@users.noreply.github.com>
2025-11-21 19:58:10 +0100

    feat : implement overView repo

--- app/src/main/java/com/android/sample/model/communication/newImplementation/overViewConv/FirestoreOverViewConvRepository.kt
+++ app/src/main/java/com/android/sample/model/communication/newImplementation/overViewConv/FirestoreOverViewConvRepository.kt
@@ -1,7 +1,29 @@
-package com.android.sample.model.communication.newImplementation.overViewConv
+package com.android.sample.model.communication.newImplementation.overview
 
-class FirestoreOverViewConvRepository : OverViewConvRepository {
+import com.android.sample.model.communication.newImplementation.overViewConv.OverViewConvRepository
+import com.android.sample.model.communication.newImplementation.overViewConv.OverViewConversation
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
+const val OVERVIEW_CONV_COLLECTION_PATH = "overViewConv"
+
+class FirestoreOverViewConvRepository(
+    db: FirebaseFirestore,
+) : OverViewConvRepository {
+
+  private val overViewRef = db.collection(OVERVIEW_CONV_COLLECTION_PATH)
+
   override suspend fun getOverViewConvUser(userId: String): List<OverViewConversation> {
-    TODO("Not yet implemented")
+    require(userId.isNotBlank()) { "User ID cannot be blank" }
+
+    val snapshotCreator = overViewRef.whereEqualTo("convCreatorId", userId).get().await()
+
+    val snapshotOther = overViewRef.whereEqualTo("otherPersonId", userId).get().await()
+
+    val allOverviews =
+        snapshotCreator.toObjects(OverViewConversation::class.java) +
+            snapshotOther.toObjects(OverViewConversation::class.java)
+
+    return allOverviews.sortedByDescending { it.lastMsg.createdAt.time }
   }
 }
--- app/src/main/java/com/android/sample/model/communication/newImplementation/overViewConv/OverViewConvRepositoryProvider.kt
+++ app/src/main/java/com/android/sample/model/communication/newImplementation/overViewConv/OverViewConvRepositoryProvider.kt
@@ -2,13 +2,16 @@ package com.android.sample.model.communication.newImplementation.overViewConv
 
 import android.content.Context
 import com.android.sample.model.RepositoryProvider
+import com.android.sample.model.communication.newImplementation.overview.FirestoreOverViewConvRepository
+import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.firestore
 
 object OverViewConvRepositoryProvider : RepositoryProvider<OverViewConvRepository>() {
   override fun init(context: Context, useEmulator: Boolean) {
     if (FirebaseApp.getApps(context).isEmpty()) {
       FirebaseApp.initializeApp(context)
     }
-    _repository = FirestoreOverViewConvRepository()
+    _repository = FirestoreOverViewConvRepository(db = Firebase.firestore)
   }
 }
