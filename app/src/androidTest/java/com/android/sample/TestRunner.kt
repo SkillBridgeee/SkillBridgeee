5eb565e4026c6b0be90e96dfe335d3514948b49e
nahuelArthur <160842802+nahuelArthur@users.noreply.github.com>
2025-11-23 19:41:49 +0100

    test : change emulator logic to pass CI

--- app/src/androidTest/java/com/android/sample/TestRunner.kt
+++ app/src/androidTest/java/com/android/sample/TestRunner.kt
@@ -3,13 +3,17 @@ package com.android.sample
 import android.os.Bundle
 import androidx.test.runner.AndroidJUnitRunner
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 class TestRunner : AndroidJUnitRunner() {
   override fun onCreate(arguments: Bundle?) {
-    // Must be called before any Firestore usage
+    super.onCreate(arguments)
+
+    // ⚠ Must be before any Firestore instance is used
     val firestore = FirebaseFirestore.getInstance()
     firestore.useEmulator("10.0.2.2", 8080)
 
-    super.onCreate(arguments)
+    val settings = FirebaseFirestoreSettings.Builder().setPersistenceEnabled(false).build()
+    firestore.firestoreSettings = settings
   }
 }
--- app/src/androidTest/java/com/android/sample/utils/TestFirestore.kt
+++ app/src/androidTest/java/com/android/sample/utils/TestFirestore.kt
@@ -1,23 +1,9 @@
 package com.android.sample.utils
 
-import android.util.Log
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreSettings
 
 object TestFirestore {
-
-  // Singleton Firestore instance, lazy-initialized
   val db: FirebaseFirestore by lazy {
-    Log.i("TestFirestore", "Getting Firestore instance for tests...")
-
-    val instance = FirebaseFirestore.getInstance()
-
-    // Disable persistence for deterministic tests
-    val settings = FirebaseFirestoreSettings.Builder().setPersistenceEnabled(false).build()
-    instance.firestoreSettings = settings
-
-    Log.i("TestFirestore", "Firestore instance ready for tests.")
-
-    instance
+    FirebaseFirestore.getInstance() // simple singleton, déjà configuré par TestRunner
   }
 }
