5e4da5aaf5d51cf5ce132e4f577ea8ad6f553941
nahuelArthur <160842802+nahuelArthur@users.noreply.github.com>
2025-11-23 20:26:04 +0100

    docs : add comment for OverViewConversation

--- app/src/main/java/com/android/sample/model/communication/newImplementation/overViewConv/OverViewConversation.kt
+++ app/src/main/java/com/android/sample/model/communication/newImplementation/overViewConv/OverViewConversation.kt
@@ -2,6 +2,20 @@ package com.android.sample.model.communication.newImplementation.overViewConv
 
 import com.android.sample.model.communication.newImplementation.conversation.MessageNew
 
+/**
+ * Represents a summary or overview of a conversation for a user.
+ *
+ * This class is used to display a list of conversations in a user interface, showing the most
+ * important information without loading the full message history.
+ *
+ * @property overViewId Unique identifier for this overview record.
+ * @property linkedConvId The ID of the actual conversation this overview is linked to.
+ * @property convName The display name of the conversation (e.g., contact or group name).
+ * @property lastMsg The last message sent or received in this conversation.
+ * @property nonReadMsgNumber The number of unread messages for the current user.
+ * @property convCreatorId The ID of the user who created the conversation.
+ * @property otherPersonId The ID of the other participant in the conversation (for 1:1 chats).
+ */
 data class OverViewConversation(
     val overViewId: String = "",
     val linkedConvId: String = "",
