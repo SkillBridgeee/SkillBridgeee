c92fae00bb97f1447d4fbe4990a0eea8cff0f4f5
NedenSinir <123alperozyurt@gmail.com>
2025-11-27 22:06:47 +0100

    fix: removing unused import

--- app/src/main/java/com/android/sample/ui/communication/DiscussionScreen.kt
+++ app/src/main/java/com/android/sample/ui/communication/DiscussionScreen.kt
@@ -1,6 +1,5 @@
 package com.android.sample.ui.communication
 
-import android.R.attr.contentDescription
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
